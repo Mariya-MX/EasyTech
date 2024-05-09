64cd9845c04e2db4eee4f4660911c02980607fec
Mariya-MX <93935212+Mariya-MX@users.noreply.github.com>
2024-05-09 10:40:05 +0530

    changed

--- repair/repair/settings.py
+++ repair/repair/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sz)0mt*tl^iuazqjp=oofx-82_8fto!37d)!mfgnken4qx+aa5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
